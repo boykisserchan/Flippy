02907ed416f5ed31b7b73a3dd56bf6f005361120
Shayaan <shayaandoodekula950@gmail.com>
2025-08-18 09:45:24 +0530

    main menu + fixes + l2 + shit + uwu + meow + things

--- player.gd
+++ player.gd
@@ -1,35 +1,42 @@
 extends CharacterBody2D
 
 
-const SPEED = 500.0
-const JUMP_VELOCITY = -500.0
-const JUMP_AMOUNT = 2
-var remainingJumps = JUMP_AMOUNT
+const SPEED = 300
 var gravityModifier = 1;
+var lvl = 1;
+const LEVEL_COUNT = 2
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("flip"):
-		gravityModifier *= -1;
-		velocity.y = 5000 * gravityModifier;
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("reset") or position.y < -1500 or position.y > 1500:
+		get_tree().reload_current_scene()
+	if position.x >= 960:
+		lvl += 1;
+		if (lvl > LEVEL_COUNT):
+			lvl = 1
+			get_tree().change_scene_to_file("res://main_menu.tscn")
+			print("uwu main")
+		else: 	
+			get_tree().change_scene_to_file("res://L" + str(lvl) + ".tscn")
+			print("uwu lvl " + str(lvl))
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	
+	if Input.is_action_just_pressed("flip") and (is_on_floor()):
+		gravityModifier *= -1;
+		velocity.y = 1000 * gravityModifier;
+		rotate(deg_to_rad(180));
+		up_direction *= -1;
+	
 	velocity += get_gravity() * delta * gravityModifier
-	if is_on_floor():
-		remainingJumps = JUMP_AMOUNT
-
-	# Handle jump
-	if (Input.is_action_just_pressed("jump") or (is_on_floor() and Input.is_action_pressed("jump"))) and remainingJumps != 0:
-		velocity.y = JUMP_VELOCITY * gravityModifier
-		remainingJumps -= 1;
 		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * SPEED * gravityModifier
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
--- tile_map_layer.gd
+++ tile_map_layer.gd
@@ -1,15 +1,11 @@
-extends Button
+extends TileMapLayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-	pressed.connect(
-		func():
-			get_tree().change_scene_to_file("res://L1.tscn")
-	)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	pass
